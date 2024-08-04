83680df96fa403a990c3115ce7b24e1b665a6585
Ricardo Hernández <ricardo.her.mendez@gmail.com>
2024-08-04 15:20:17 -0600

    develop cart button and cart view to display selected products to purchase

--- app/navigation/ProductStack.tsx
+++ app/navigation/ProductStack.tsx
@@ -1,16 +1,48 @@
 import { createNativeStackNavigator, NativeStackScreenProps } from "@react-navigation/native-stack"
 import Products from "../screens/Products";
 import ProductDetails from "../screens/ProductDetails";
+import CartModal from "../screens/CartModal";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
+import { Ionicons } from '@expo/vector-icons';
+import useCartStore from "../state/cartStore";
+import { useEffect, useState } from "react";
+import { Text, TouchableOpacity, View, StyleSheet } from "react-native";
 
 type ProductsStackParamList = {
   Products: undefined,
   ProductDetails: { id: number }
-  //TODO: Cart modal
+  CartModal: undefined
 }
 
 const ProductsStack = createNativeStackNavigator<ProductsStackParamList>();
+
 export type ProductsScreenProps = NativeStackScreenProps<ProductsStackParamList, 'Products'>
 export type ProductsDetailsScreenProps = NativeStackScreenProps<ProductsStackParamList, 'ProductDetails'>
+export type StackNavigation = NavigationProp<ProductsStackParamList>
+
+const CartButton = () => {
+  const navigation = useNavigation<StackNavigation>()
+  const { products } = useCartStore((state) => ({ products: state.products }))
+
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const count = products.reduce((prev, products) => prev + products.quantity, 0);
+    setCount(count);
+  }, [products]);
+
+  return (
+    <TouchableOpacity
+      onPress={() => {
+        navigation.navigate('CartModal');
+      }}>
+      <View style={styles.countContainer}>
+        <Text style={styles.countText}>{count}</Text>
+      </View>
+      <Ionicons name="cart" size={28} color={'#000'} />
+    </TouchableOpacity>
+  );
+}
 
 const ProductsStackNav = () => {
   return (
@@ -18,7 +50,8 @@ const ProductsStackNav = () => {
       headerStyle: {
         backgroundColor: '#1FE687'
       },
-      headerTintColor: '#141414'
+      headerTintColor: '#141414',
+      headerRight: () => <CartButton />
     }}>
       <ProductsStack.Screen
         name="Products"
@@ -34,8 +67,35 @@ const ProductsStackNav = () => {
           headerTitle: ''
         }}
       />
+      <ProductsStack.Screen
+        name="CartModal"
+        component={CartModal}
+        options={{
+          headerShown: false,
+          presentation: 'modal'
+        }}
+      />
     </ProductsStack.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  countContainer: {
+    position: 'absolute',
+    zIndex: 1,
+    bottom: -5,
+    right: -10,
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  countText: {
+    fontSize: 12,
+    fontWeight: 'bold',
+  },
+});
 
 export default ProductsStackNav
